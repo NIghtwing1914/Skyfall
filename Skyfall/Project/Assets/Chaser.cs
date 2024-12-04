17b6294cceb22756b2ec692ec7966ef0da8bad00
Ayush Anand <ayush_anand@iitgn.ac.in>
2024-12-04 20:34:28 +0530

    Pushing files

---
+++ Skyfall/Project/Assets/Chaser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Chaser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    NavMeshAgent agent;
+    Transform target;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
---
+++ Skyfall/Project/Assets/timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text timertext;
+    private float starttimer;
+    void Start()
+    {
+        starttimer = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time - starttimer;
+        float display = 80.0f - t;
+        timertext.text = "Time remaining  " + display.ToString("f2");
+
+    }
+}
